74a13ab70662cfbf03f5d88e6497a003a9757193
BinjuMaharjan <bmaharjan@wvstateu.edu>
2025-04-11 00:52:36 -0400

    Add files via upload

---
+++ Activityfirst.py
@@ -0,0 +1,3 @@
+print("Hello")
+print('You', 'can', 'do', 'it', sep='-', end='\n')
+print("the result is of 5 plus two", 5 + 2)
---
+++ namevariable.py
@@ -0,0 +1,10 @@
+"""
+namevariable.py
+Purpose: This program demonstrate assigning name of variable.
+Author: Binju
+Date: April 10, 2025
+
+"""
+course = 'Python 101'
+dnaseq = 'ATGCCTGAG'
+num = 100
